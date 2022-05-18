7299ef48f6c7c15fd7debb1bd59f8db732e401d2
warren24h <71285092+warren24h@users.noreply.github.com>
2022-05-18 10:57:19 -0400

    removed import not being used

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import Section from "./Section"
-import AILinkedInDemo from "./AILinkedInDemo.mp4";
 
 function Home() {
   return (
