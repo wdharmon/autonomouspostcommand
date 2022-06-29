67c6b546828401e02cc80f781bd9b0c3f7da8482
warren24h <71285092+warren24h@users.noreply.github.com>
2022-06-29 18:15:37 -0400

    yes

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function Home() {
       <Container>
 
         <Section
-        title = "Warren Harmon"
+        title = "Warren Harmon "
         description =  "Warren Harmon is the Founder, Developer, and Web Designer for APC since 2020. As a self-proclaimed social networking geek, Warren was inspired to create a product for his peers that allow them to create professional posts without thinking. He began his career working as a Technical Support Intern with a strong interest in databases and is currently a Data Technology Analyst based out of Manhattan. Warren earned a Bachelors of Computer Engineering degree, with minors in Computer Science and Cybersecurity"
         
         />
